44a68b0d54ea1349760f6cfe68e2f0bd99ceacb7
Ahmed Shehata <AhmedShehataAbdalluh@yahoo.com>
2026-07-09 16:56:57 +0300

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.go
@@ -0,0 +1,31 @@
+import "slices"
+func threeSum(nums []int) [][]int {
+    slices.Sort(nums)
+    output := [][]int{}
+
+    for i := 0; i < len(nums)-2; i++ {
+        if i > 0 && nums[i] == nums[i-1] {
+            continue // skip duplicate anchors
+        }
+        left, right := i+1, len(nums)-1
+        for left < right {
+            sum := nums[i] + nums[left] + nums[right]
+            if sum == 0 {
+                output = append(output, []int{nums[i], nums[left], nums[right]})
+                left++
+                right--
+                for left < right && nums[left] == nums[left-1] {
+                    left++ // skip duplicates
+                }
+                for left < right && nums[right] == nums[right+1] {
+                    right-- // skip duplicates
+                }
+            } else if sum < 0 {
+                left++
+            } else {
+                right--
+            }
+        }
+    }
+    return output
+}
