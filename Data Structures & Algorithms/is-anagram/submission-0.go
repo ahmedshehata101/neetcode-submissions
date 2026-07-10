8253b578efe528e53ae81411a3efa35983004032
Ahmed Shehata <AhmedShehataAbdalluh@yahoo.com>
2026-07-10 19:14:41 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.go
@@ -0,0 +1,18 @@
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	freq := make(map[rune]int)
+	for _, ch := range s {
+		freq[ch]++
+	}
+	for _, ch := range t {
+		freq[ch]--
+		if freq[ch] < 0 {
+			return false
+		}
+	}
+	return true
+
+}
